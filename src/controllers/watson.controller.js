3019e4a9ae61484557b5d593794d54974f55d38c
MaEscalanteHe <maescalantehe@gmail.com>
2020-08-01 00:02:33 -0400

    feature(watson-intents) Added 1 plus intent to webhook-watson.

--- src/controllers/watson.controller.js
+++ src/controllers/watson.controller.js
@@ -1,12 +1,16 @@
 // Dialogs
-const bienvenida = require('../watson/bienvenida');
-const despedida = require('../watson/despedida');
+const bienvenida = require('../watson/wellcome');
+const despedida = require('../watson/goodbye');
+const estadoDesafios = require('../watson/challenge_status');
 
 class WatsonController {
 	async postWebhook(req, res) {
+		console.log(req.body);
+
 		try {
 			bienvenida(req, res);
 			despedida(req, res);
+			estadoDesafios(req, res);
 		} catch (error) {
 			console.log(error);
 		}
